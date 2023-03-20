1c054e47ee467b40b11a900f8a5fb39086602e80
Martin Apiwat <martin.apiwat@gmail.com>
2023-03-20 16:34:55 +0100

    added board and tickets

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import Board from "./components/Board/Board";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 export default function App() {
   return (
     <div className="main-container">
       <Sidebar />
-      <div>Hej</div>
+      <Board />
     </div>
   );
 }
---
+++ src/components/Ticket/Ticket.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Ticket.scss";
+
+type Props = {};
+
+export default function Ticket({}: Props) {
+  return (
+    <div className="ticket">
+      <h3>This is a ticket</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p>3 subtasks</p>
+    </div>
+  );
+}
