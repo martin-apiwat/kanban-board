05666b72116414fb6e0e46e3fbc531df218dad1a
Martin Apiwat <martin.apiwat@gmail.com>
2023-03-21 17:01:16 +0100

    started on dymanic tickets

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,39 +1,26 @@
 import Ticket from "../Ticket/Ticket";
 import "./Board.scss";
+import { useBoardContext } from "../../context/BoardsContext";
 
 type Props = {};
 
 export default function Board({}: Props) {
+  const { currentProject } = useBoardContext();
+
   return (
     <div className="board">
-      <div className="board-column">
-        <div className="board-title">
-          <div className="board-title-icon todo"></div>
-          <h4 className="">Todo</h4>
+      {currentProject.board.map((column) => (
+        <div className="board-column">
+          <div className="board-title">
+            <div className="board-title-icon todo"></div>
+            <h4 className="">Todo</h4>
+          </div>
+
+          {column.tickets.map((ticket) => (
+            <Ticket />
+          ))}
         </div>
-        <Ticket />
-        <Ticket />
-        <Ticket />
-      </div>
-      <div className="board-column">
-        <div className="board-title">
-          <div className="board-title-icon doing"></div>
-          <h4 className="">Doing</h4>
-        </div>
-        <Ticket />
-        <Ticket />
-      </div>
-      <div className="board-column">
-        <div className="board-title">
-          <div className="board-title-icon done"></div>
-          <h4 className="">Done</h4>
-        </div>
-        <Ticket />
-        <Ticket />
-        <Ticket />
-        <Ticket />
-        <Ticket />
-      </div>
+      ))}
     </div>
   );
 }
--- src/context/BoardsContext.tsx
+++ src/context/BoardsContext.tsx
@@ -1,10 +1,6 @@
 import React, { createContext, useContext, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-
-interface Project {
-  name: string;
-  id: string;
-}
+import { Project, projectsData } from "./projectsData";
 
 interface Context {
   projects: Project[];
@@ -29,16 +25,23 @@ type Props = {
 };
 
 export default function BoardsContextProvider({ children }: Props) {
-  const [projects, setProjects] = useState<Project[]>([
-    { name: "hej", id: uuidv4() },
-    { name: "då", id: uuidv4() },
-  ]);
-
+  const [projects, setProjects] = useState<Project[]>(projectsData);
   const [selectedIndex, setSelectedIndex] = useState(0);
   const currentProject = projects[selectedIndex];
 
   function createProject(name: string) {
-    setProjects([...projects, { name, id: uuidv4() }]);
+    setProjects([
+      ...projects,
+      {
+        name,
+        id: uuidv4(),
+        board: [
+          { name: "Todo", tickets: [] },
+          { name: "Doing", tickets: [] },
+          { name: "Done", tickets: [] },
+        ],
+      },
+    ]);
     changeBoard(projects.length);
   }
 
