2bc982ba2f3572bb89cf2d4341f535f76f30baf9
Martin Apiwat <martin.apiwat@gmail.com>
2023-03-22 15:31:48 +0100

    added toggle function on add task and close

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 import Board from "./components/Board/Board";
 import CreateTaskModel from "./components/CreateTaskModel/CreateTaskModel";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 export default function App() {
+  const [showCreateTast, setShowCreateTask] = useState(false);
+
+  function toggleShowCreateTask() {
+    setShowCreateTask(!showCreateTast);
+  }
+
   return (
     <div className="app">
-      <Navbar />
+      <Navbar toggleShowCreateTask={toggleShowCreateTask} />
       <div className="main-container">
         <Sidebar />
         <Board />
       </div>
-      <CreateTaskModel />
+      {showCreateTast && (
+        <CreateTaskModel toggleShowCreateTask={toggleShowCreateTask} />
+      )}
     </div>
   );
 }
--- src/components/CreateTaskModel/CreateTaskModel.tsx
+++ src/components/CreateTaskModel/CreateTaskModel.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import "./CreateTaskModel.scss";
 import { X } from "react-feather";
 
-export default function CreateTaskModel() {
+type Props = {
+  toggleShowCreateTask: () => void;
+};
+
+export default function CreateTaskModel({ toggleShowCreateTask }: Props) {
   return (
     <div className="model-container">
       <div className="model-card">
-        <div className="model-close">
+        <div className="model-close" onClick={() => toggleShowCreateTask()}>
           <X />
         </div>
+        <h3 className="model-title">Create new ticket</h3>
       </div>
     </div>
   );
